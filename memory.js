623a38e2c9f006225760ca487317bc2a01aeca77
Rob Colby <rcolby@me.com>
2013-05-02 16:10:41 -0700

    Got our cards and rows.

--- memory.js
+++ memory.js
@@ -8,14 +8,28 @@ function Memory(size) {
   function initialize() {
     _.each(_.shuffle(subset), function(i) {that.matrix.push(i);});
     _.each(_.shuffle(subset), function(i) {that.matrix.push(i);});
-    _.each(subset, function(i) {that.winningCombos.push([indexOf, lastIndexOf]);}
+    _.each(subset, function(i) {
+      that.winningCombos.push([_.indexOf(that.matrix, i), _.lastIndexOf(that.matrix, i)]);
+    });
   }
   initialize();
   return that;
 }
 
+function draw(memory) {
+//  var row = $('<div class="row"></div>');
+  var card;
+  _.each(memory.matrix, function(letter, index, list){
+    card = $('<div class="card"></div>');
+    card.attr('id', 'card-' + index);
+    card.html(letter);
+    $('#board').append(card);
+  });
+}
+
 $(document).ready(function(){
   var small = 10;
   var m = new Memory(small);
+  draw(m);
 
 });
